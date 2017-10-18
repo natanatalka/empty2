05b96b44c438c40b1357a4707dca529b0ea84006
PowWow Admin <admin@studio.dev.powwowinc.net>
2017-10-18 16:14:44 +0300

    [Studio] Initialize application

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -1,15 +1,16 @@
 /***  Generated file, do not change.  */
 import { Page } from 'ionic-angular/navigation/nav-util';
-import { MenuComponent } from './menu/menu.component';
-import { Blank_PhonePortrait } from '../pages/Blank/PhonePortrait/Blank';
+import { MenuComponent } from "./menu/menu.component";
+
 export class Screens {
+
   static declarations: Page[] = [
-    MenuComponent,
-    Blank_PhonePortrait
+    MenuComponent
   ];
+
   static mapping = {
-    'Blank': {
-      PhonePortrait: Blank_PhonePortrait
-    }
   }
+
 }
+
+
--- client/src/mocks/index.ts
+++ client/src/mocks/index.ts
@@ -1,1 +1,1 @@
-export * from './Blank/index';
+export {};
